5e65bb0a8c52cd52fd4a43c8bb1f36bb3bdd7d35
Paula Origlio <minplynn@aol.com>
2015-09-17 16:11:36 -0600

    angular working

--- script.js
+++ script.js
@@ -2,23 +2,27 @@ angular.module("practiceModule", [])
 
 angular.module("practiceModule").controller("practiceController", ["$scope",function($scope){
 
-	$scope.randomInfo = {
-		heading : "Name goes here",
-		one		: "Hey, I say a different thing!",
-		two		: "And another different thing!",
-		three   : "How impressive..."
+	// $scope.randomInfo = {
+	// 	heading : "Name goes here",
+	// 	one		: "Hey, I say a different thing!",
+	// 	two		: "And another different thing!",
+	// 	three   : "How impressive..."
+	// }
+
+	$scope.randomInfo = ["Name goes here", "doesn't matter", "blah", "index"]
+
+	$scope.booleanIndex = [false,false,false,false]
+
+	$scope.clickText = function ( $index ) {
+		$scope.booleanIndex[$index] = true;
 	}
 
-	$scope.clickText = function ( $event ) {
-		$scope.boxVisibility = true;
+	$scope.focused = function ( $index ) {
+		$scope.booleanIndex[$index] = true;
 	}
 
-	$scope.focused = function ( $event ) {
-		$scope.boxVisibility = true;
-	}
-
-	$scope.blurred = function ( $event ) {
-		$scope.boxVisibility = false;
+	$scope.blurred = function ( $index ) {
+		$scope.booleanIndex[$index] = false;
 	}
 
 }])
